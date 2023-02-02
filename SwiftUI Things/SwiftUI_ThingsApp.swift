867b9acddfbb2a4bfda48d932b0bffd868ed1c1a
Javier Laguna <javier.laguna@innocv.com>
2023-02-02 13:15:11 +0100

    Initial Commit

---
+++ SwiftUI Things/SwiftUI_ThingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ThingsApp.swift
+//  SwiftUI Things
+//
+//  Created by Javier Laguna on 2/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ThingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
