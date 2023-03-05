279ae3f39bd3d62f26ba99474ac5ca5266dc2f52
Javier Laguna <javier.laguna@innocv.com>
2023-03-05 12:39:52 +0100

    Add LongPressGestureExample

---
+++ SwiftUI Things/Containers/LongPressGestureExample.swift
@@ -0,0 +1,34 @@
+
+import SwiftUI
+
+struct LongPressGestureExample: View {
+    
+    @GestureState private var press = false
+    @State private var show = false
+    
+    var body: some View {
+        Image(systemName: "camera.fill")
+            .foregroundColor(.white)
+            .frame(width: 60, height: 60)
+            .background(show ? Color.black : Color.blue)
+            .mask(Circle())
+            .scaleEffect(press ? 2 : 1)
+            .animation(.spring(response: 0.4, dampingFraction: 0.6))
+            .gesture(
+                LongPressGesture(minimumDuration: 0.5)
+                    .updating($press) { currentState, gestureState, transaction in
+                        gestureState = currentState
+                    }
+                    .onEnded { value in
+                        show.toggle()
+                    }
+            )
+    }
+}
+
+struct LongPressGestureExample_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        LongPressGestureExample()
+    }
+}
