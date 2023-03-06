ee00aa09d22e335f691083692c70b58111dd2e0e
Javier Laguna <javier.laguna@innocv.com>
2023-03-06 19:16:10 +0100

    Add View+hideKeyboard extension

---
+++ SwiftUI Things/Utilities/View+hideKeyboard.swift
@@ -0,0 +1,10 @@
+
+import SwiftUI
+
+extension View {
+    
+    func hideKeyboard() {
+        let resign = #selector(UIResponder.resignFirstResponder)
+        UIApplication.shared.sendAction(resign, to: nil, from: nil, for: nil)
+    }
+}
