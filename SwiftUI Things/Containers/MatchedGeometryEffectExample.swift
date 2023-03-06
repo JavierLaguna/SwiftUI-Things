33470bab0f0a535350e0d04f7ab1e91a1471391b
Javier Laguna <javier.laguna@innocv.com>
2023-03-06 18:30:12 +0100

    Improve MatchedGeometryEffectExample with a real ui case

--- SwiftUI Things/Containers/MatchedGeometryEffectExample.swift
+++ SwiftUI Things/Containers/MatchedGeometryEffectExample.swift
@@ -1,47 +1,88 @@
 
 import SwiftUI
 
+/***
+ It is not recommended to use Matched Geometry Effect on containers like
+ VStack, HStack and ZStack. Instead, you should use it on elements like shapes, texts and buttons.
+ */
 struct MatchedGeometryEffectExample: View {
-   
-    @State private var show = false
+    
+    private struct Page: Identifiable {
+        let id = UUID()
+        let title: String
+        let imageName: String
+    }
+    
+    private let pages = [
+        Page(title: "Front", imageName: "magazine-front-cover"),
+        Page(title: "Back", imageName: "magazine-back-cover")
+    ]
+    
     @Namespace private var namespace
-
+    @State private var selectedPage: Page?
+    
+    private func titleMGEId(_ page: Page) -> String {
+        "title \(page.title)"
+    }
+    
+    private func imageMGEId(_ page: Page) -> String {
+        "image \(page.title)"
+    }
+    
+    private func changeSelectedPage(page: Page?) {
+        withAnimation(.spring(response: 0.5, dampingFraction: 0.7)) {
+            selectedPage = page
+        }
+    }
+    
     var body: some View {
         ZStack {
-            if !show {
-                ScrollView {
-                    HStack {
-                        VStack {
-                            Text("Title").foregroundColor(.white)
-                                .matchedGeometryEffect(id: "title", in: namespace)
-                        }
-                        .frame(width: 100, height: 100)
-                        .background(
-                            Rectangle().matchedGeometryEffect(id: "shape", in: namespace)
-                        )
-                        Rectangle()
-                            .frame(width: 100, height: 100)
-                        Spacer()
-                    }
-                    .padding(8)
+            if let selectedPage {
+                VStack() {
+                    Text(selectedPage.title)
+                        .font(.title)
+                        .underline()
+                        .padding()
+                        .matchedGeometryEffect(id: titleMGEId(selectedPage), in: namespace)
+                    
+                    Image(selectedPage.imageName)
+                        .resizable()
+                        .scaledToFill()
+                        .matchedGeometryEffect(id: imageMGEId(selectedPage), in: namespace)
+                }
+                .frame(maxWidth: .infinity, maxHeight: 400)
+                .onTapGesture {
+                    changeSelectedPage(page: nil)
                 }
                 
             } else {
-                VStack {
-                    Text("Title 2").foregroundColor(.white)
-                        .matchedGeometryEffect(id: "title", in: namespace)
-                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
-                        .padding()
+                ScrollView {
+                    HStack {
+                        ForEach(pages) { page in
+                            Image(page.imageName)
+                                .resizable()
+                                .scaledToFill()
+                                .matchedGeometryEffect(id: imageMGEId(page), in: namespace)
+                                .frame(width: 100, height: 100)
+                                .overlay(alignment: .bottom) {
+                                    Text(page.title)
+                                        .font(.caption2)
+                                        .foregroundColor(.white)
+                                        .padding(4)
+                                        .background(.ultraThinMaterial)
+                                        .clipShape(RoundedRectangle(cornerRadius: 4))
+                                        .matchedGeometryEffect(id: titleMGEId(page), in: namespace)
+                                }
+                                .onTapGesture {
+                                    changeSelectedPage(page: page)
+                                }
+                        }
+                        
+                        Spacer()
+                    }
+                    .padding(.vertical, 16)
+                    .padding(.horizontal, 8)
                 }
-                .frame(maxWidth: .infinity, maxHeight: 400)
-                .background(
-                    Rectangle().matchedGeometryEffect(id: "shape", in: namespace)
-                )
-            }
-        }
-        .onTapGesture {
-            withAnimation(.spring(response: 0.5, dampingFraction: 0.7)) {
-                show.toggle()
             }
         }
     }
---
+++ SwiftUI Things/Containers/PopoverExample.swift
@@ -0,0 +1,20 @@
+//
+//  PopoverExample.swift
+//  SwiftUI Things
+//
+//  Created by Javier Laguna on 6/3/23.
+//
+
+import SwiftUI
+
+struct PopoverExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PopoverExample_Previews: PreviewProvider {
+    static var previews: some View {
+        PopoverExample()
+    }
+}
